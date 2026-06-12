2dd14dd8320e82f9381a6eada413dfed9ee58b38
Kalverine <khalidaras1458@gmail.com>
2026-06-11 22:09:40 -0400

    Implement initial structure for MyHashMap
    Added initial fields and methods for MyHashMap class.

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,5 +1,21 @@
 import java.util.LinkedList;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+private static int DEFAULT_INITIAL_CAPACITY = 4;
+private static int MAXIMUM_CAPACITY =1 << 30;
+private int capacity;
+  private float loadFactorThreshold;
+  private int size = 0;
+  private LinkedList<Entry<K, V>>[] table;
+
+  @Override
+  public V put(K key, V value) {
+    return null;
+}
+
+@Override
+  public V get(K key) {
+    return null;
+  }
 
 }
