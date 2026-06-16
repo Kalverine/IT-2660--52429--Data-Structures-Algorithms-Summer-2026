95f87d938736727caf240a3241f6ef160fb179a0
Kalverine <khalidaras1458@gmail.com>
2026-06-16 09:35:04 -0400

    Refactor MyHashMap initialization and table structure
    Refactor MyHashMap to simplify capacity and size initialization. Update table to use LinkedList.

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,21 +1,8 @@
 import java.util.LinkedList;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-private static int DEFAULT_INITIAL_CAPACITY = 4;
-private static int MAXIMUM_CAPACITY =1 << 30;
-private int capacity;
-  private float loadFactorThreshold;
-  private int size = 0;
-  private LinkedList<Entry<K, V>>[] table;
-
-  @Override
-  public V put(K key, V value) {
-    return null;
-}
-
-@Override
-  public V get(K key) {
-    return null;
-  }
-
-}
+private int capacity = 4, size = 0;
+  private LinkedLost<MyMap.Entry<K, V>>[] table = new LinkedList[capacity];
+  
+@Override public V put(K k, V v) {
+  int i = k.hashCode() & (capacity - 1);
